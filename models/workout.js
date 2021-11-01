d3c1e1b552dcd0e33cfbabf7b592088ad554c1d6
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-31 20:59:17 -0400

    Added comments to model schema and export of object

--- models/workout.js
+++ models/workout.js
@@ -2,29 +2,40 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema ({
+const ExerciseSchema = new Schema ({
+    //Expect either cardio or workout. user interface is using select
     activity: {
         type: String,
         required: "Must chose an exercise type."
     },
+    //User can define. Either activity should be named
     name: {
         type: String,
         required: "Must identify the exercise performed."
     },
+    //Either activity should always have a time
     duration: {
         type: Number,
         required: "Please provide time spent performing activity."
     },
+    //expect cardio and mobility action
     distance: {
         type: Number,
     },
+    //expect workout activity and exercise name
     weight: {
         type: Number,
     },
+    //expect workout, each weight amount belongs to a set
     sets: {
         type: Number,
     },
+    //expect workout, belongs with set 
     reps: {
         type: Number,
     }
-})
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
