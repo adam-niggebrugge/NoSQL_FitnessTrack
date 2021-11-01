c6391f3357a68dad407684e40f0f0006d85c77fe
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-01 17:29:40 -0400

    index to connect all models

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const ResistanceExercise = require('./ResistanceExercise');
+const CardioExercise = require('./CardioExercise');
+const Workout = require('./Workout');
+
+module.exports = { ResistanceExercise, CardioExercise, Workout};
