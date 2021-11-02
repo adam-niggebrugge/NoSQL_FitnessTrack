4c4619e7c9721310bcb71968dc751e9ec6beafdb
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-02 13:08:41 -0400

    formatting update and async sexiness used throughout

--- routes/api.js
+++ routes/api.js
@@ -3,42 +3,40 @@ const { Workout } = require("../models");
 
 
 //retrieve a workout
-router.get("/workouts", (req, res) => {
-    console.log(`api router workkouts, are there request parts? ${req.params}`)
-    Workout.find({})
-    .then(dbWorkoutRetrieved => {
-      res.status(200).json(dbWorkoutRetrieved);
-    })
-    .catch(err => {
-      console.log(`${err}`)
+router.get("/workouts", async (req, res) => {
+  try {
+    const workout  = await Workout.find({})
+    res.status(200).json(workout);
+  } catch (err) {  
+      console.log(`${err}`);
       res.status(400).json(err);
-    });
-})
+  };
+});
 
 //add a dependency, exercise to a workout. can be many
 router.put("/workouts/:id", async (req, res) => {
   try{
-      const workout = await Workout.findByIdAndUpdate(
-          {_id: req.params.id},
-          {$push: {exercises: req.body}
-            });
-        res.status(200).json(workout);
-    } catch (err) {
-      res.status(400).json(err);
-    }
+    const workout = await Workout.findByIdAndUpdate(
+      {_id: req.params.id},
+      {$push: {exercises: req.body}}
+    );
+    res.status(200).json(workout);
+  } catch (err) {
+    console.log(`${err}`);
+    res.status(400).json(err);
+  }
 })
 
 //create a workout
-router.post("/workouts/", (req, res) => {
-    Workout.create(req.body)
-    .then(dbWorkoutCreated => {
-      res.json(dbWorkoutCreated);
-    })
-    .catch(err => {
-      console.log(`${err}`)
+router.post("/workouts/", async (req, res) => {
+  try {
+    const workout = await Workout.create(req.body)
+    res.status(200).json(workout);
+  } catch { err =>
+      console.log(`${err}`);
       res.status(400).json(err);
-    });
-})
+  };
+});
 
 //get workouts over time period
 router.get("/workouts/range", async (req, res) => {
@@ -51,6 +49,6 @@ router.get("/workouts/range", async (req, res) => {
     } catch { err => 
       res.status(400).json(err);
     }
-})
+});
 
 module.exports = router;
