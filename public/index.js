0bd48cf4c5505e322dc3d3512a610c01fea5e267
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-02 15:24:32 -0400

    more console logs being removed

--- public/index.js
+++ public/index.js
@@ -5,10 +5,8 @@ async function init() {
   if (location.search.split("=")[1] === undefined) {
     const workout = await API.getLastWorkout();
     if (workout) {
-      console.log(`Just a simple log in index init ${workout}`)
       location.search = "?id=" + workout._id;
     } else {
-      console.log(`Else call within init(), workout undefined or something`)
       document.querySelector("#continue-btn").classList.add("d-none")
     }
   }
