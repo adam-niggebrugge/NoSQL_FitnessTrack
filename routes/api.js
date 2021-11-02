cfd6641cbc5c8ae6ffff79841752d3e0474c35b5
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-11-02 11:56:20 -0400

    another spot requiring plural form of exercise

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,7 @@ router.put("/workouts/:id", async (req, res) => {
   try{
       const workout = await Workout.findByIdAndUpdate(
           {_id: req.params.id},
-          {$push: {exercise: req.body}
+          {$push: {exercises: req.body}
             });
         res.status(200).json(workout);
     } catch (err) {
